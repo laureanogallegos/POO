858c303a06679d8f22a7e123f53ce9489d0b0b05
Laureano Gallegos <laureanogallegos@cetcom.com.ar>
2023-04-12 10:48:14 -0300

    Ejercicio caja de ahorro 11/04

---
+++ Unidad1/Clase2/CajaAhorro.cs
@@ -0,0 +1,68 @@
+﻿namespace Unidad1.Clase2
+{
+    internal class CajaAhorro
+    {
+        private int nroCuenta;
+        private int saldo;
+        private string titular;
+        private TipoMonedaEnum tipoMoneda;
+        public enum TipoMonedaEnum { Dolares, Pesos };// el enum permite seleccionar entre los valores definidos dentro de la enumaeracion
+
+
+        public int NroCuenta
+        {
+            get { return nroCuenta; }
+            set { nroCuenta = value; }
+        }
+        public int Saldo
+        {
+            get { return saldo; }
+        }
+
+        public string Titular
+        {
+            get { return titular; }
+            set { titular = value; }
+        }
+
+        public TipoMonedaEnum TipoMoneda
+        { 
+            get { return tipoMoneda; }
+            set { tipoMoneda = value; }
+        }
+
+        public string Extraer(int monto)
+        {
+            if (monto < 0)
+            {
+                return "El monto: " + monto + "debe ser mayor que cero."; //Forma mas comun de concatenar strings
+            }
+            else if (monto <= saldo)
+            {
+                saldo -= monto;
+                return $"Se ha extraido {monto} {tipoMoneda}";
+            }
+            else
+                return "No hay saldo suficiente para realizar la extracción";// cuando solamente tengo una sola instruccion dentro del if o el else
+                                                                             // no es necesario ponerlo entre llaves
+        }
+
+        public string Depositar(int monto)
+        {
+            if (monto < 0)
+            {
+                return "El monto: " + monto + "debe ser mayor que cero."; //Forma mas comun de concatenar strings
+            }
+            else 
+            {
+                saldo += monto;
+                return $"Se ha depositado {monto} {tipoMoneda}";   
+            }
+        }
+
+        public string ConsultarSaldo()
+        {
+            return $"El saldo en {tipoMoneda} es de :{saldo}"; //este tipo de concatenacion se denomina string interpolation
+        }
+    }
+}
--- Unidad1/Program.cs
+++ Unidad1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unidad1.Clase2;
 
 namespace Unidad1
 {
@@ -10,6 +11,77 @@ namespace Unidad1
     {
         static void Main(string[] args)
         {
+           // Clase1();
+
+            Clase2();// creo metodos para separar cada ejercicio del main y no hacer un main monolitito
+
+
+        }
+
+        private static void Clase2()
+        {
+            Console.WriteLine("Bienvenido al banco UAI");
+            CajaAhorro cajaAhorro = new CajaAhorro();
+            Console.WriteLine("Ingrese su numero de cuenta");
+            cajaAhorro.NroCuenta = Convert.ToInt32(Console.ReadLine());// convierto un string a int32 (int)
+            Console.WriteLine("Ingrese su nombre y apellido");
+            cajaAhorro.Titular = Console.ReadLine(); // no necesito realizar conversion ya que readline devuelve un string
+            Console.WriteLine("Ingrese el tipo de moneda: Pesos|Dolares");
+            bool conversionOk = true;
+            do
+            {
+                var conversionOK = Enum.TryParse(Console.ReadLine(), out CajaAhorro.TipoMonedaEnum tipoMoneda);
+                if (conversionOK)
+                    cajaAhorro.TipoMoneda = tipoMoneda;
+            } while (!conversionOk);
+            Console.WriteLine("Seleccione una Opcion:");
+            Console.WriteLine("1-Extraer:");
+            Console.WriteLine("2-Depositar:");
+            Console.WriteLine("3-Saldo:");
+            Console.WriteLine("4-Salir");
+            
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese el monto a extraer");
+                        var montoExtraccion = Convert.ToInt32(Console.ReadLine());
+                        var mensajeExtraccion = cajaAhorro.Extraer(montoExtraccion);
+                        Console.WriteLine(mensajeExtraccion);
+                        break;
+                    case 2:
+                        Console.WriteLine("Ingrese el monto a depositar");
+                        var montoDeposito = Convert.ToInt32(Console.ReadLine());
+                        var mensajeDeposito = cajaAhorro.Depositar(montoDeposito);
+                        Console.WriteLine(mensajeDeposito);
+                        break;
+                        case 3:
+                        //var mensajeSaldo = cajaAhorro.Saldo();
+                        var mensajeSaldo = cajaAhorro.ConsultarSaldo();// puedo usar un metodo o la propiedad
+                        Console.WriteLine(mensajeSaldo);
+                        break;
+                }
+                Console.WriteLine("Presione cualquier tecla para continuar");
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("Seleccione una Opcion:");
+                Console.WriteLine("1-Extraer:");
+                Console.WriteLine("2-Depositar:");
+                Console.WriteLine("3-Saldo:");
+                Console.WriteLine("4-Salir");
+
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+            } while (opcion >0 && opcion < 4);
+            
+            Console.ReadKey();// Esto evita que se cierre el programa
+        }
+
+        private static void Clase1()
+        {
+            /*
             //Tipo nombreDeObjeto = new Tipo();
             Clase clase1 = new Clase();
             //Ejemplo de propiedad Lecto/Escritura
@@ -19,11 +91,10 @@ namespace Unidad1
             //Ejemplo de propiedad de solo lectura
             var valor2 = clase1.Propiedad;
             clase1.Propiedad2 = "";
-            //
             //Propiedad de solo escritura
-            clase1.Propiedad3= valor2;
+            clase1.Propiedad3 = valor2;
             var valor3 = clase1.Propiedad3;
-
+            */
         }
     }
 }
