1ce7b80b81181bd72072980b4f371b0581f829c6
Laureano Gallegos <laureanogallegos@cetcom.com.ar>
2023-04-13 10:28:11 -0300

    Caja de ahorro con Titular

---
+++ Unidad1/Clase2/Ejemplo2/Titular.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidad1.Clase2.Ejemplo2
+{
+    internal class Titular
+    {
+        private string nombre;
+        private string apellido;
+        private DateTime FechaNacimiento;
+        private int dni
+    }
+}
